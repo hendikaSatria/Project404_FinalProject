8dfa1fb4c3776fba82ba15e0082969b97885e460
ilzammuhtaromi3003 <ilzammuhtaromi3003@gmail.com>
2023-11-24 17:32:25 +0700

    hapus yang tidak digunakan

--- e-commerce/backend/routes/index.js
+++ e-commerce/backend/routes/index.js
@@ -1,9 +1,5 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< Updated upstream
-// const homeController = require("../controllers/homeController");
-=======
->>>>>>> Stashed changes
 const adminRouter = require("./admin");
 const productRouter = require("./product");
 const userRouter = require("./user");
@@ -11,10 +7,6 @@ const orderRouter = require("./order");
 
 
 
-<<<<<<< Updated upstream
-router.use("/", userRouter);
-=======
->>>>>>> Stashed changes
 router.use("/admin", adminRouter);
 router.use("/product", productRouter);
 router.use("/user", userRouter);
